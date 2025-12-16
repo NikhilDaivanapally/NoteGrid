2c3d28eded0a6bbf746fd84d7d17009b3e696ffd
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-12-16 13:17:15 +0530

    chore(db): setup database with Mongoose singleton and integrate with better-auth

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,5 +1,14 @@
 import { betterAuth } from "better-auth";
+import connectToDatabase from "../mongoose";
+import mongoose from "mongoose";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { Db } from "mongodb";
+
+await connectToDatabase();
+
+const client = mongoose?.connection.getClient();
+const db = mongoose.connection.db as Db;
 
 export const auth = betterAuth({
-  //...
+  database: mongodbAdapter(db, { client }),
 });
---
+++ src/lib/mongoose.ts
@@ -0,0 +1,49 @@
+// <mongoose.ts>
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+// Ensure the cache is initialized on the global object
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase(): Promise<typeof mongoose> {
+  // If we already have a connection, return it
+  if (cached!.conn) return cached!.conn;
+
+  // If we don't have a promise yet, create one
+  if (!cached!.promise) {
+    const opts = { bufferCommands: false };
+
+    cached!.promise = mongoose.connect(MONGODB_URI!, opts).then((m) => {
+      console.log("✅ MongoDB Connected via Mongoose");
+      return m;
+    });
+  }
+
+  try {
+    cached!.conn = await cached!.promise;
+  } catch (e) {
+    cached!.promise = null; // Reset promise on error so we can try again
+    throw e;
+  }
+
+  return cached!.conn;
+}
+
+export default connectToDatabase;
