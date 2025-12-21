a6630376c1dd1ff824986258560f78f9ed8284f4
NikhilDaivanapally <nikhildaivanapally@gmail.com>
2025-12-21 20:22:18 +0530

    feat(auth): add user session fetching via RTK Query and API route
    - added userApi session query using RTK Query
    - implemented authenticated user session API endpoint
    - enabled cached user session state across the app

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -25,7 +25,7 @@ export function NavUser({
   user: {
     name: string;
     email: string;
-    avatar: string;
+    image: string;
   };
 }) {
   const { isMobile } = useSidebar();
@@ -40,8 +40,18 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">N</AvatarFallback>
+                {user.image ? (
+                  <>
+                    <AvatarImage src={user.image} alt={user.name} />
+                    <AvatarFallback className="rounded-lg">
+                      {user.name}
+                    </AvatarFallback>
+                  </>
+                ) : (
+                  <AvatarFallback className="rounded-lg">
+                    {user.name.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                )}
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{user.name}</span>
@@ -58,10 +68,18 @@ export function NavUser({
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">N</AvatarFallback>
-                </Avatar>
+                {user.image ? (
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage src={user.image} alt={user.name} />
+                    <AvatarFallback className="rounded-lg">
+                      {user.name}
+                    </AvatarFallback>
+                  </Avatar>
+                ) : (
+                  <div className="w-8 h-8 rounded-lg flex items-center justify-center">
+                    {user.name.charAt(0).toUpperCase()}
+                  </div>
+                )}
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
                   <span className="truncate text-xs">{user.email}</span>
@@ -76,21 +94,6 @@ export function NavUser({
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            {/* <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup> */}
-            {/* <DropdownMenuSeparator /> */}
             <DropdownMenuItem>
               <LogOut />
               Log out
